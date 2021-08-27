c173727c3061f82e0ed04e58d750c7c084f895ca
willy <shen.willy95@gmail.com>
2021-08-27 14:43:48 +0700

    solhint

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,14 +1,14 @@
 import { ethers } from "hardhat";
-import {
-  Greeter__factory
-} from "../typechain";
+import { Greeter__factory } from "../typechain";
 
 import chai from "chai";
 const { expect } = chai;
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const GreeterFactory = <Greeter__factory>await ethers.getContractFactory("Greeter");
+    const GreeterFactory = <Greeter__factory>(
+      await ethers.getContractFactory("Greeter")
+    );
     const greeter = await GreeterFactory.deploy("Hello, world!");
     await greeter.deployed();
 
