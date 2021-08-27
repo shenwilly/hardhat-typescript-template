caafb2a6bd8b502d61b7101873b1d886a21b0fb3
willy <shen.willy95@gmail.com>
2021-08-27 15:43:48 +0700

    config: update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,24 @@
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
-import '@typechain/hardhat'
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-etherscan";
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 import { HardhatUserConfig } from "hardhat/types";
+import {
+  ETHERSCAN_API_KEY,
+  MAINNET_PRIVATE_KEY,
+  NETWORK_FORK_URL,
+  NETWORK_MAINNET_URL,
+  NETWORK_RINKEBY_URL,
+  NETWORK_ROPSTEN_URL,
+  RINKEBY_PRIVATE_KEY,
+  ROPSTEN_PRIVATE_KEY,
+} from "./constants";
 
 import "./tasks/accounts";
 
@@ -16,14 +26,14 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
     compilers: [
-      { 
-        version: "0.8.4", 
+      {
+        version: "0.8.4",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
+            runs: 200,
           },
-        } 
+        },
       },
     ],
   },
@@ -31,43 +41,35 @@ const config: HardhatUserConfig = {
     hardhat: {
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
       allowUnlimitedContractSize: true,
-      // forking: {
-      //   url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      //   blockNumber: 12984971,
-      // },
+      forking: {
+        url: NETWORK_FORK_URL,
+        blockNumber: 12984971,
+      },
       // hardfork: "berlin"
     },
-    localhost: {
-      allowUnlimitedContractSize: true
+    mainnet: {
+      url: NETWORK_MAINNET_URL,
+      accounts: [MAINNET_PRIVATE_KEY],
     },
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-    //   accounts: [MAINNET_PRIVATE_KEY],
-    // },
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-    //   accounts: [RINKEBY_PRIVATE_KEY],
-    // },
-    // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-    //   accounts: [ROPSTEN_PRIVATE_KEY],
-    // },
+    rinkeby: {
+      url: NETWORK_RINKEBY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+    ropsten: {
+      url: NETWORK_ROPSTEN_URL,
+      accounts: [ROPSTEN_PRIVATE_KEY],
+    },
   },
-  // etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    // apiKey: ETHERSCAN_API_KEY,
-  // },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   gasReporter: {
     enabled: false,
-    currency: 'eth',
-  },
-  mocha: {
-    timeout: 2000000
+    currency: "eth",
   },
   typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
+    outDir: "typechain",
+    target: "ethers-v5",
   },
 };
 
